8973df05a57a88f98b1299ebcda665d86f36560f
João Pedro Carvalho <joaopedro.nc@outlook.com>
2022-08-02 11:28:02 -0300

    Initial commit

---
+++ reactjs-rocketseat/iniciando-react/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Tweet } from "./components/tweet";
+
+import "./App.css";
+import { AppRoutes } from "./Routes";
+
+
+function App() {
+    const [tweets, setTweets] = useState<string[]>([
+      "Tweet 1",
+      "Tweet 2",
+      "Tweet 3",
+      "Tweet 4"
+    ]);
+
+    function createTweet() {
+      setTweets([...tweets, "Tweet 5"]);
+    }
+
+  return (
+
+    
+    <div>
+      <AppRoutes />
+      {tweets.map(tweet => {
+        return <Tweet text={tweet} />
+      })}
+
+      <button 
+      onClick={createTweet}
+
+      style={{
+        backgroundColor: '#8257e6',
+        border: 0,
+        padding: '6px 12px',
+        color: '#fff'
+      }}
+      >
+        Adicionar Tweet
+      </button>
+    </div>
+  );
+}
+
+export default App;
