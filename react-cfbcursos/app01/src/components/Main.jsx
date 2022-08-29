70f2fa931f0fa466ae87d6843bac63f843c1fc3e
João Pedro Carvalho <joaopedro.nc@outlook.com>
2022-08-29 15:30:38 -0300

    CSS em React

--- react-cfbcursos/app01/src/components/Main.jsx
+++ react-cfbcursos/app01/src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Data from "./Data";
+import './Main.css'
 
 function App() {
   const Canal = () => {
@@ -11,13 +12,18 @@ function App() {
   const curso = "Curso de ReactJs";
   const Soma = (n1, n2) => n1 + n2;
 
-  con
+  const textoDestaque = {
+    color: '#00f',
+    fontSize: '3em',
+  }
 
   return (
-    <div>
+    <div className="container">
       <h2 style={{color: '#f00', fontSize: 20}}>Curso de React</h2>
-      <p>Se inscreva em nosso canal</p>
+      <p style={textoDestaque}>Se inscreva em nosso canal</p>
+      <p className="texto">Curso React</p>
       {/* <Data canal={Canal} youtube={youtube} curso={curso} soma={Soma} /> */}
+      <a href="#" className="link" target={'_blank'}>CFB Cursos</a>
     </div>
   );
 }
